4e11e947b77d3c149ac9c564fec75ee92dbadb46
aakashnnt123 <aakash5427896@gmail.com>
2025-05-25 15:48:57 +0530

    Add Logo

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+
+
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
 
-export default Login
+const[emailId , setemailId] = useState("");
+const[password , setpassword] = useState("");
+
+const handleLogin = async () => {
+   try{
+    const res = await axios.post("http://localhost:3000/login",{
+      emailId,
+      password,
+  });
+   }catch(err){
+    console.log(err);
+   } 
+};
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card card-dash bg-base-300 w-96 ">
+        <div className="card-body">
+          <h1 className="card-title justify-center">Login</h1>
+          <div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email_Id</legend>
+              <input type="text" 
+              value={emailId} 
+              className="input" placeholder="Enter your Email_id.." 
+               onChange={(e)=> setemailId(e.target.value)}
+               />
+            </fieldset>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password</legend>
+              <input type="password"
+               value={password} 
+               className="input" placeholder="Enter your Password.." 
+               onChange={(e)=> setpassword(e.target.value)}
+               />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,36 +1,56 @@
-import React from 'react'
+import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="navbar bg-base-300 shadow-sm">
-    <div className="flex-1">
-      <a className="btn btn-ghost text-xl">👨‍💻Dev_Tinder</a>
-    </div>
-    <div className="flex gap-2">
-      <div className="dropdown dropdown-end mx-10">
-        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-          <div className="w-10 rounded-full">
-            <img
-              alt="Tailwind CSS Navbar component"
-              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+    <div className="navbar bg-black text-primary-content shadow-md px-6">
+      <div className="flex-1 flex  items-center gap-4">
+        <a className="text-2xl flex items-center gap-2">
+          <img
+            className="w-14 h-14 rounded-[4px] border-2 border-accent"
+            src="Utils/Logo.png"
+            alt="DevMate Logo"
+          />
+          <span className="font-extrabold tracking-widest bg-gradient-to-r from-pink-500 via-yellow-400 to-green-400 text-transparent bg-clip-text">
+            DevMate
+          </span>
+        </a>
+      </div>
+
+      {/* Right: Avatar Dropdown */}
+      <div className="flex gap-2">
+        <div className="dropdown dropdown-end mx-4">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar border-0 hover:border-0 hover:bg-transparent"
+          >
+            <div className="w-14 rounded-md">
+              <img
+                alt="User Avatar"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
           </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 text-neutral rounded-box z-10 mt-3 w-52 p-2 shadow-lg border border-base-300"
+          >
+            <li>
+              <a className="justify-between">
+                Profile <span className="badge badge-accent">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
         </div>
-        <ul
-          tabIndex={0}
-          className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-          <li>
-            <a className="justify-between">
-              Profile
-              <span className="badge">New</span>
-            </a>
-          </li>
-          <li><a>Settings</a></li>
-          <li><a>Logout</a></li>
-        </ul>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
 export default NavBar;
